14095590c198b9643ed644bd31c95fa84ad4639d
Bexruz <bexruzasqarov08@gmail.com>
2025-01-02 22:25:40 +0500

    Delete adminapp/urls.py

--- adminapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home_page, logout_page, login_page, SignUpView
-
-urlpatterns = [
-    path('', home_page, name="home_page"),
-    path('login_page/', login_page, name="login_page"),
-    path('logout_page/', logout_page, name="logout_page"),
-    path('signup/', SignUpView.as_view(), name="signup"),
-]
